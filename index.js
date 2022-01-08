9443b444feb0e866d758ec16d057c75b60299d9f
nakayoshi11 <yoshikithi.10etoile@gmail.com>
2022-01-08 16:16:04 +0900

    list名表示commit

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-window.addEventListener('load', function(){
+function pullDown() {
 
   const pullDownButton = document.getElementById("lists")
   const pullDownParents = document.getElementById("pull-down")
-  
-    pullDownButton.addEventListener('mouseover', function(){
-      this.setAttribute("style", "background-color:#FFBEDA;")
-    })
-  
-    pullDownButton.addEventListener('mouseout', function(){
-      this.removeAttribute("style", "background-color:#FFBEDA;")
-    })
-  
-    pullDownButton.addEventListener('click', function() {
-      // プルダウンメニューの表示と非表示の設定
-      if (pullDownParents.getAttribute("style") == "display:block;") {
-        // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
-        pullDownParents.removeAttribute("style", "display:block;")
-      } else {
-        // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
-        pullDownParents.setAttribute("style", "display:block;")
-      }
+  const pullDownChild = document.querySelectorAll(".pull-down-list")
+  const currentList = document.getElementById("current-list")
+
+  pullDownButton.addEventListener('mouseover', function(){
+    this.setAttribute("style", "background-color:#FFBEDA;")
+  })
+
+  pullDownButton.addEventListener('mouseout', function(){
+    this.removeAttribute("style", "background-color:#FFBEDA;")
+  })
+
+  pullDownButton.addEventListener('click', function() {
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      pullDownParents.removeAttribute("style", "display:block;")
+    } else {
+      pullDownParents.setAttribute("style", "display:block;")
+    }
+  })
+
+  pullDownChild.forEach(function(list) {
+    list.addEventListener('click', function() {
+      const value = list.innerHTML
+      currentList.innerHTML = value
     })
   })
+}
+
+window.addEventListener('load', pullDown)
