2340ea992daaa78d800acc67c22405b80150687e
nakayoshi11 <yoshikithi.10etoile@gmail.com>
2022-01-08 14:32:26 +0900

    list追加commit

---
+++ index.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function(){
+
+  const pullDownButton = document.getElementById("lists")
+  const pullDownParents = document.getElementById("pull-down")
+
+  pullDownButton.addEventListener('mouseover', function(){
+    pullDownButton.setAttribute("style", "background-color:#FFBEDA;")
+  })
+
+  pullDownButton.addEventListener('mouseout', function(){
+    pullDownButton.removeAttribute("style", "background-color:#FFBEDA;")
+  })
+
+  pullDownButton.addEventListener('click', function() {
+    pullDownParents.setAttribute("style", "display:block;")
+  })
+})
